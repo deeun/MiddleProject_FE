19fd9a191bcea3b2bcd2a1ddf3ad5db66d90cb88
YunJo0618 <104193872+YunJo0618@users.noreply.github.com>
2022-06-15 21:14:16 +0900

    Details map api

--- components/Ours/Our/Details.js
+++ components/Ours/Our/Details.js
@@ -1,36 +1,37 @@
-import React from 'react'
+import React,{useEffect} from 'react'
 import { useRouter } from 'next/router'
 import styles from '../Our/Details.module.css'
-// import styles from '../../Layout/Main/'
+import KakaoMap from "../Our/Kakaomap";
+
 
 // 체험 상세정보
 
-const Main = (props) => {
+const  Details = (props) => {
   const router = useRouter();
   const Reservation = () => {
     console.log('Reservation');
-    router.push('/ours/reservation')
+    router.push('/ours/reservation')                       
   }
-  return (
-    <>
+
+    return (
+      <>
     <div className={styles.flexbox}>
 
       <div className={styles.flexboxleft}>
-        <div class="img" id="map">
-        <img className={styles.map} src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQOOScfb_rNQSNSKmAG4YhNvRdSyBkVzjkAKw&usqp=CAU" />
-        </div>
+      <KakaoMap />
       </div>
       
       <div className={styles.flexboxright}>
         <div className={styles.info}>
-          <h3>체험지 01. 시골마을</h3>
-          <h5>시골냄새 가득 시골마을</h5>
-          <div className={styles.box1}><box>주소</box></div><br/>
-          <div className={styles.box2}><box>연락처</box></div><br/>
-          <div className={styles.box3}><box>체험명</box></div><br/>
-        
-        <button onClick = {Reservation}>예약하기</button>
+          <h1>체험지 01. 시골마을</h1>
+          <h3>시골냄새 가득 시골마을</h3>
+          <div className={styles.box1}><box>주소: </box></div><br/>
+          <div className={styles.box2}><box>연락처: </box></div><br/>
+          <div className={styles.box3}><box>체험명: </box></div><br/>
         </div>
+        
+          <button className={styles.btn} onClick = {Reservation}>예약하기</button>
+             
         </div>
         
 
@@ -41,4 +42,4 @@ const Main = (props) => {
   )
 }
 
-export default Main
+export default Details
